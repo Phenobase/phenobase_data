b307d938421f1019d67fb1ef964bd3d4a84bec2f
John Deck <jdeck88@gmail.com>
2025-05-05 15:49:15 -0700

    adding new results file

--- reasoning/april2025/run.sh
+++ reasoning/april2025/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 curl -L https://raw.githubusercontent.com/PlantPhenoOntology/ppo/refs/heads/2025-04-release/ppo.owl -o ppo.owl 
-#robot reason --reasoner HermiT -i ppo.owl -o ppo_reasoned.owl 
-robot query -i ppo_reasoned.owl --query query.sparql results.tsv 
+robot query -i ppo.owl --query query.sparql results.tsv 
 echo 'trait_id,trait_name,mapped_traits' > results.csv
 awk 'NR>1 { gsub("\t", ","); print }' results.tsv > results.csv
